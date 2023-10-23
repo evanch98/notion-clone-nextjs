93d3a94fa9c71e4cbd086a6aeac7817be49322c6
Kyaw Thu <evanch98@gmail.com>
2023-10-23 21:07:04 +0800

    update the RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Notion Clone",
   description: "The connected workspace where better, faster work happens.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
