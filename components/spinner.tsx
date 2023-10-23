35d0752bec31febbd8dbcdf51e6215d60adc9ccc
Kyaw Thu <evanch98@gmail.com>
2023-10-24 00:00:02 +0800

    update the spinnerVariants

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const spinnerVariants = cva("text-muted-foreground animate-spinner", {
+const spinnerVariants = cva("text-muted-foreground animate-spin", {
   variants: {
     size: {
       default: "h-4 w-4",
