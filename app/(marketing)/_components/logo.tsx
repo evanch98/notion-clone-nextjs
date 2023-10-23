76be8d8818ce5aeb8622d5469458083365fa456f
Kyaw Thu <evanch98@gmail.com>
2023-10-23 21:55:47 +0800

    add a new component Logo

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src="/logo.svg" height={40} width={40} alt="Logo" />
+    </div>
+  );
+};
