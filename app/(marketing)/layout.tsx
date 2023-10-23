d2b95c58ad12fdec4f26fe32d5669d8b8bbd2d77
Kyaw Thu <evanch98@gmail.com>
2023-10-23 22:08:07 +0800

    add MarketingLayout

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,10 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <Navbar />
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
