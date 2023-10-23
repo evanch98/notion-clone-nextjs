076f686a506270edaf41d155c11195f7c5f73b61
Kyaw Thu <evanch98@gmail.com>
2023-10-23 22:34:18 +0800

    update the Navbar component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,6 +3,7 @@
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const Navbar = () => {
   const scrolled = useScrollTop();
@@ -16,7 +17,7 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        Login
+        <ModeToggle />
       </div>
     </div>
   );
