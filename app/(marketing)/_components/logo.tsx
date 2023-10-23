9ca4a278d667bb8e5445f86135e41e0f238fd972
Kyaw Thu <evanch98@gmail.com>
2023-10-23 22:35:36 +0800

    update the Logo component

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -10,7 +10,20 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
-      <Image src="/logo.svg" height={40} width={40} alt="Logo" />
+      <Image
+        src="/logo.svg"
+        height={40}
+        width={40}
+        alt="Logo"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        height={40}
+        width={40}
+        alt="Logo"
+        className="hidden dark:block"
+      />
       <p className={cn("font-semibold", font.className)}>Notion</p>
     </div>
   );
