eeb5f7de8fc1ccc9900034270a0f65159df18ba2
Kyaw Thu <evanch98@gmail.com>
2023-10-23 21:22:02 +0800

    update the Heading component

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
 export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
-      <h1>
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
         <span className="underline">Notion</span>
       </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Notion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      <Button>
+        Enter Notion <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
     </div>
   );
 };
