150d0d5895e6b1ab2183063de7efc769c6380cd5
Kyaw Thu <evanch98@gmail.com>
2023-10-23 21:23:39 +0800

    add the Heroes component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
+        <Heroes />
       </div>
     </div>
   );
