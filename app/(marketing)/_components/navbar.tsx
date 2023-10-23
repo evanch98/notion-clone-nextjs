c476d94d4218f1ecbefd88a16553f78e39821f52
Kyaw Thu <evanch98@gmail.com>
2023-10-23 22:23:42 +0800

    update the Navbar component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,5 +1,23 @@
 "use client";
 
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import { Logo } from "./logo";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const scrolled = useScrollTop();
+
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        Login
+      </div>
+    </div>
+  );
 };
