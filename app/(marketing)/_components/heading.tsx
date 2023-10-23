ad5fe67b1c5733ad36bf3b3df4e91f33e0f42e16
Kyaw Thu <evanch98@gmail.com>
2023-10-23 21:18:41 +0800

    add a new component Heading

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Notion</span>
+      </h1>
+    </div>
+  );
+};
