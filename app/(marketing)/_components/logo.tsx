ef8175105326a3ca78d03aa51deec9d8d51edd1f
Kyaw Thu <evanch98@gmail.com>
2023-10-23 21:57:30 +0800

    update the Logo component

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -11,6 +11,7 @@ export const Logo = () => {
   return (
     <div className="hidden md:flex items-center gap-x-2">
       <Image src="/logo.svg" height={40} width={40} alt="Logo" />
+      <p className={cn("font-semibold", font.className)}>Notion</p>
     </div>
   );
 };
