a691d4c1292f71786770dad965c517361825de87
Kyaw Thu <evanch98@gmail.com>
2023-10-23 21:49:37 +0800

    update the Heroes component

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -1,5 +1,26 @@
 import Image from "next/image";
 
 export const Heroes = () => {
-  return <div>Heroes</div>;
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+          <Image
+            src="/documents.png"
+            fill
+            alt="Documents"
+            className="object-contain"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden md:block">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain"
+            alt="Reading"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
