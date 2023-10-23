d90f913e34a4030626033d5ed410d3e4e9d86c97
Kyaw Thu <evanch98@gmail.com>
2023-10-23 22:40:40 +0800

    update the Navbar component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
   return (
     <div
       className={cn(
-        "z-50 bg-background fixed top-0 flex items-center w-full p-6",
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
