5364cc715628b3d12908dd1b61be883a6c5f2e1f
Kyaw Thu <evanch98@gmail.com>
2023-10-24 14:09:18 +0800

    update the Navigation component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,22 +1,38 @@
 "use client";
 
-import { ChevronsLeft } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 export const Navigation = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
   const [isResetting, setIsResetting] = useState(false);
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   return (
     <>
-      <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
+      >
         {/* To collapse the sidebar */}
         <div
           role="button"
-          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+          className={cn(
+            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100"
+          )}
         >
           <ChevronsLeft className="h-6 w-6" />
         </div>
@@ -29,6 +45,20 @@ export const Navigation = () => {
         {/* To resize the sidebar */}
         <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "left-0 w-full"
+        )}
+      >
+        <nav className="bg-transparent px-3 py-2 w-full">
+          {isCollapsed && (
+            <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
+          )}
+        </nav>
+      </div>
     </>
   );
 };
