61cf321bd6888e121340f8d550da10ddf852f861
Kyaw Thu <evanch98@gmail.com>
2023-10-24 12:48:12 +0800

    update the Navigation component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import { ChevronsLeft } from "lucide-react";
+
 export const Navigation = () => {
   return (
     <>
       <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+        {/* To collapse the sidebar */}
+        <div
+          role="button"
+          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
         <div>
           <p>Action items</p>
         </div>
         <div className="mt-4">
           <p>Documents</p>
         </div>
+        {/* To resize the sidebar */}
         <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
     </>
