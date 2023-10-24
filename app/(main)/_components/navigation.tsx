a08a468b9d84c618ee2d0dde74f6aa5d577f8d6d
Kyaw Thu <evanch98@gmail.com>
2023-10-24 12:45:04 +0800

    update the Navigation component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,5 +1,17 @@
 "use client";
 
 export const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <>
+      <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+      </aside>
+    </>
+  );
 };
