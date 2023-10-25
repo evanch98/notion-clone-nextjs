33074a1a5a379a9181ee69e7ad2efe42ecc82ebd
Kyaw Thu <evanch98@gmail.com>
2023-10-26 00:26:08 +0800

    add a new function archive

--- convex/documents.ts
+++ convex/documents.ts
@@ -2,6 +2,54 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+// to set the isArchived to true
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    // a recursive function to set the isArchived of the children to true
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      // get the children
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      // loop through children to set isArchived to true
+      for (const child of children) {
+        await ctx.db.patch(child._id, { isArchived: true });
+        await recursiveArchive(child._id); // to loop through the children of the children
+      }
+    };
+
+    // set the isArchived to true
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+
+    return document;
+  },
+});
+
 // get the documents for the sidebar
 export const getSidebar = query({
   args: {
