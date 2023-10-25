98f2574c1f05d54a1da9d27395792318c6cc8f64
Kyaw Thu <evanch98@gmail.com>
2023-10-25 13:00:18 +0800

    import the DocumentList component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -16,6 +16,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
