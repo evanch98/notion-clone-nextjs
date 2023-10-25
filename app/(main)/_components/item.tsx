9466959055f7ac7570d1c6aaec577bf238106b8a
Kyaw Thu <evanch98@gmail.com>
2023-10-25 11:44:11 +0800

    update the Item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,5 +1,23 @@
 "use client";
 
-export const Item = () => {
-  return <div>Item</div>;
+import { LucideIcon } from "lucide-react";
+
+interface ItemProps {
+  label: string;
+  onClick: () => void;
+  icon: LucideIcon;
+}
+
+export const Item = ({ label, onClick, icon: Icon }: ItemProps) => {
+  return (
+    <div
+      onClick={onClick}
+      role="button"
+      style={{ paddingLeft: "12px" }}
+      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+    >
+      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      <span className="truncate">{label}</span>
+    </div>
+  );
 };
