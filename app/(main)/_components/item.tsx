8738f74bcf8e4fbebb01cdddb5e74ba78f241b5c
Kyaw Thu <evanch98@gmail.com>
2023-10-25 12:03:56 +0800

    update the Item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { Id } from "@/convex/_generated/dataModel";
-import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
 
 interface ItemProps {
   id?: Id<"documents">;
   documentIcon?: string;
   active?: boolean;
-  expanded?: string;
+  expanded?: string; // expanded = see the children of the document
   isSearched?: boolean;
-  level?: number;
+  level?: number; // to use it to set the padding of the document
   onExpand?: () => void;
   label: string;
   onClick: () => void;
@@ -28,15 +29,41 @@ export const Item = ({
   onClick,
   icon: Icon,
 }: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight; // change the icon based of the "expanded" prop
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary"
+      )}
     >
-      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      {/* Render this part if and only if the item has an id (documents id) */}
+      {!!id && (
+        <div
+          onClick={() => {}}
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+        >
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
+        </div>
+      )}
+      {/* Render the appropriate icon based on the presence of documentIcon */}
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
+      {/* Render this part if and only if the "isSearched" prop is true */}
+      {isSearched && (
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">Ctrl</span>k
+        </kbd>
+      )}
     </div>
   );
 };
