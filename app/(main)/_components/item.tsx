2c2ef570b21216c75193e3a5d67d54d9a37ede3d
Kyaw Thu <evanch98@gmail.com>
2023-10-25 12:56:13 +0800

    update the item.tsx

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -67,3 +67,9 @@ export const Item = ({
     </div>
   );
 };
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div style={{ paddingLeft: level ? `${level * 12 + 25}px` : "12px" }}></div>
+  );
+};
