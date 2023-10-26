4428dda31d6f7e12293bb04615293806b6167dcf
Kyaw Thu <evanch98@gmail.com>
2023-10-26 12:27:46 +0800

    add remove function

--- convex/documents.ts
+++ convex/documents.ts
@@ -189,3 +189,31 @@ export const restore = mutation({
     return existingDocument;
   },
 });
+
+// ro actually remove the document from the database
+export const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.delete(args.id);
+
+    return document;
+  },
+});
