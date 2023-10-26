64c10f2e6385d366e16cfa80cddd5a4cf8f75b88
Kyaw Thu <evanch98@gmail.com>
2023-10-26 12:09:46 +0800

    add getTrash function

--- convex/documents.ts
+++ convex/documents.ts
@@ -105,3 +105,25 @@ export const create = mutation({
     return document;
   },
 });
+
+// to get the archived documents
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user_parent", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArchived"), true))
+      .order("desc")
+      .collect();
+
+    return documents;
+  },
+});
