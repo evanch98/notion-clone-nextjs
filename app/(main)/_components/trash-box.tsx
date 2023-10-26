5ebbde4779e9f515badaa083bbeb8ec97846fa8d
Kyaw Thu <evanch98@gmail.com>
2023-10-26 14:21:18 +0800

    update the TrashBox component

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,5 +1,65 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+
 export const TrashBox = () => {
+  const router = useRouter();
+  const params = useParams();
+  const documents = useQuery(api.documents.getTrash);
+  const restore = useMutation(api.documents.restore);
+  const remove = useMutation(api.documents.remove);
+
+  const [search, setSearch] = useState("");
+
+  const filteredDocuments = documents?.filter((document) => {
+    return document.title.toLowerCase().includes(search.toLowerCase());
+  });
+
+  const onClick = (documentId: string) => {
+    router.push(`/documents/${documentId}`);
+  };
+
+  const onRestore = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    documentId: Id<"documents">
+  ) => {
+    event.stopPropagation();
+    const promise = restore({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored!",
+      error: "Failed to restore note.",
+    });
+  };
+
+  const onRemove = (documentId: Id<"documents">) => {
+    const promise = remove({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Deleting note...",
+      success: "Note deleted!",
+      error: "Failed to delete note.",
+    });
+
+    if (params.documentId === documentId) {
+      router.push("/documents");
+    }
+  };
+
+  if (documents === undefined) {
+    return (
+      <div className="h-full flex items-center justify-center p-4">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
   return <div>Trash Box</div>;
 };
