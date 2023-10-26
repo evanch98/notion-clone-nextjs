4f5637435b5186713f1d6868776b98d949dfbc45
Kyaw Thu <evanch98@gmail.com>
2023-10-26 12:23:32 +0800

    add restore function

--- convex/documents.ts
+++ convex/documents.ts
@@ -127,3 +127,65 @@ export const getTrash = query({
     return documents;
   },
 });
+
+// to restore the archived documents
+// the logic is to detach the child from the parent if the parent is archived and
+// restore all the children
+export const restore = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    // to restore the children of the document being restored
+    const recursiveRestore = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: false,
+        });
+
+        await recursiveRestore(child._id);
+      }
+    };
+
+    const options: Partial<Doc<"documents">> = {
+      isArchived: false,
+    };
+
+    // if the parent is archived, detach the document from the parent
+    if (existingDocument.parentDocument) {
+      const parent = await ctx.db.get(existingDocument.parentDocument);
+      if (parent?.isArchived) {
+        options.parentDocument = undefined;
+      }
+    }
+
+    await ctx.db.patch(args.id, options);
+
+    recursiveRestore(args.id);
+
+    return existingDocument;
+  },
+});
