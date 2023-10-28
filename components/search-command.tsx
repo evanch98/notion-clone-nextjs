fe155ebfa1499adc848e066715065fd723cbf31a
Kyaw Thu <evanch98@gmail.com>
2023-10-28 12:37:09 +0800

    update the SearchCommand

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -52,28 +52,27 @@ export const SearchCommand = () => {
 
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
-      <CommandInput placeholder={`Search ${user?.fullName}'s Notion...`}>
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Documents">
-            {documents?.map((document) => (
-              <CommandItem
-                key={document._id}
-                value={`${document._id}-${document.title}`}
-                title={document.title}
-                onSelect={onSelect}
-              >
-                {document.icon ? (
-                  <p className="mr-2 text-[18px]">{document.icon}</p>
-                ) : (
-                  <File className="mr-2 h-4 w-4" />
-                )}
-                <span>{document.title}</span>
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      </CommandInput>
+      <CommandInput placeholder={`Search ${user?.fullName}'s Notion...`} />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Documents">
+          {documents?.map((document) => (
+            <CommandItem
+              key={document._id}
+              value={`${document._id}-${document.title}`}
+              title={document.title}
+              onSelect={onSelect}
+            >
+              {document.icon ? (
+                <p className="mr-2 text-[18px]">{document.icon}</p>
+              ) : (
+                <File className="mr-2 h-4 w-4" />
+              )}
+              <span>{document.title}</span>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
     </CommandDialog>
   );
 };
