9bed4ef7a77b4ff12c7c069ba32200085fc0cd4e
Kyaw Thu <evanch98@gmail.com>
2023-10-28 11:58:35 +0800

    add SearchCommand

---
+++ components/search-command.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { api } from "@/convex/_generated/api";
+import { useSearch } from "@/hooks/use-search";
+import { useUser } from "@clerk/clerk-react";
+import { useQuery } from "convex/react";
+import { File } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export const SearchCommand = () => {
+  const { user } = useUser();
+  const router = useRouter();
+};
