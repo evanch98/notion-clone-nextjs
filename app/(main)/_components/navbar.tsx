77ac2f5a5def9c94d0c6e18f5fbb254ccf0e5313
Kyaw Thu <evanch98@gmail.com>
2023-10-29 14:31:48 +0800

    update the Navbar component

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,3 +1,10 @@
-export const Navbar = () => {
+"use client";
+
+interface NavbarProps {
+  isCollapsed: boolean;
+  onResetWidth: () => void;
+}
+
+export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   return <div>Navbar!</div>;
 };
