10e4ad81ca68d325c5ed4b173dfe47343c23378e
Kyaw Thu <evanch98@gmail.com>
2023-10-29 15:58:07 +0800

    update the Navbar component

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -20,7 +20,11 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   });
 
   if (document === undefined) {
-    return <p>Loading</p>;
+    return (
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+        <Title.Skeleton />
+      </nav>
+    );
   }
 
   if (document === null) {
