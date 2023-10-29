df80b965c3630e727261ff30cccce79cfa0ab4b1
Kyaw Thu <evanch98@gmail.com>
2023-10-29 12:34:21 +0800

    update the Item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -112,7 +112,7 @@ export const Item = ({
         <div
           onClick={handleExpand}
           role="button"
-          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
