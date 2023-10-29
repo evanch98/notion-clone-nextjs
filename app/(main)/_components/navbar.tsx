6d2bdd9d654d3b48cc4029b295911a63b2a29c3d
Kyaw Thu <evanch98@gmail.com>
2023-10-29 15:02:20 +0800

    update the Navbar component

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,10 +1,45 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
+import { useParams } from "next/navigation";
+
 interface NavbarProps {
   isCollapsed: boolean;
   onResetWidth: () => void;
 }
 
 export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
-  return <div>Navbar!</div>;
+  const params = useParams();
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if (document === undefined) {
+    return <p>Loading</p>;
+  }
+
+  if (document === null) {
+    return null;
+  }
+
+  return (
+    <>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
+      </nav>
+    </>
+  );
 };
