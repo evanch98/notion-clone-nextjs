a6eb38d8a79ddf12939c228cf8215c084e00f8df
Kyaw Thu <evanch98@gmail.com>
2023-10-29 12:22:41 +0800

    add ModalProvider

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { SettingsModal } from "@/components/modals/settings-modal";
+import { useEffect, useState } from "react";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <SettingsModal />
+    </>
+  );
+};
