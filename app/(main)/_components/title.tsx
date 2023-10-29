f577b789795c65104a0f11f2ce02ffcfea239a82
Kyaw Thu <evanch98@gmail.com>
2023-10-29 15:51:58 +0800

    add Title.Skeleton

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
 import { useMutation } from "convex/react";
@@ -67,4 +68,8 @@ export const Title = ({ initialData }: TitleProps) => {
       )}
     </div>
   );
+};
+
+Title.Skeleton = function TitleSkeleton() {
+  return <Skeleton className="h-9 w-16 rounded-md" />;
 };
