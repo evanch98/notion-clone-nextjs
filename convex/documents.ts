41eb2f5ea2beb0aa4bf0b7c0dbff587ee84fd95e
Kyaw Thu <evanch98@gmail.com>
2023-10-29 15:12:23 +0800

    add new function update

--- convex/documents.ts
+++ convex/documents.ts
@@ -268,3 +268,39 @@ export const getById = query({
     return document;
   },
 });
+
+export const update = mutation({
+  args: {
+    id: v.id("documents"),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthenticated");
+    }
+
+    const userId = identity.subject;
+
+    const { id, ...rest } = args;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.patch(args.id, { ...rest });
+
+    return document;
+  },
+});
