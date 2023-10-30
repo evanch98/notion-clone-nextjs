711729a2c7c737934827ee0ec2667fcf2618fcef
Kyaw Thu <evanch98@gmail.com>
2023-10-30 12:06:42 +0800

    update the Navbar component

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -44,6 +44,9 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
