41a44b25b525c2a331917cfc291b9b9f67deac16
Kyaw Thu <evanch98@gmail.com>
2023-10-30 18:20:58 +0800

    update the DocumentIdPage

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -26,6 +26,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-40">
+      <div className="h-[35vh]" />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
       </div>
