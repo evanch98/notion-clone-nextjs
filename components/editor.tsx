49a9535fa261423e46d17aa7fd18ba0e9d3f29c1
Kyaw Thu <evanch98@gmail.com>
2023-10-31 23:18:34 +0800

    add a new component Editor

---
+++ components/editor.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  initialContent?: string;
+  editable?: boolean;
+}
+
+export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+  return <div>Editor</div>;
+};
