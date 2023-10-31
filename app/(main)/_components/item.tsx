40ab3e3559daa111335d9bf0d30bf32bd710b9a8
Kyaw Thu <evanch98@gmail.com>
2023-11-01 00:02:57 +0800

    update the Item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -59,7 +59,7 @@ export const Item = ({
     event.stopPropagation();
     if (!id) return;
 
-    const promise = archive({ id });
+    const promise = archive({ id }).then(() => router.push("/documents"));
     toast.promise(promise, {
       loading: "Moving to trash...",
       success: "Note moved to trash!",
@@ -84,7 +84,7 @@ export const Item = ({
         if (!expanded) {
           onExpand?.();
         }
-        // router.push(`/documents/${documentId}`);
+        router.push(`/documents/${documentId}`);
       }
     );
 
