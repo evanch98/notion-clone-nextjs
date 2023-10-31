d4eed098cffc2095018c5eedb0a9d36135424a5f
Kyaw Thu <evanch98@gmail.com>
2023-10-31 11:47:32 +0800

    add CoverImageModal

---
+++ components/modals/cover-image-modal.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import { useCoverImage } from "@/hooks/use-cover-image";
+
+export const CoverImageModal = () => {
+  const coverImage = useCoverImage();
+
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">Cover Image</h2>
+        </DialogHeader>
+        <div>TODO: Upload image</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
