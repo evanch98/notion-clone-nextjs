643befafad6cf254ee229cbc04b17af9869a398c
Kyaw Thu <evanch98@gmail.com>
2023-11-01 00:03:35 +0800

    update the Editor component

--- components/editor.tsx
+++ components/editor.tsx
@@ -12,7 +12,7 @@ interface EditorProps {
   editable?: boolean;
 }
 
-export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
   const { edgestore } = useEdgeStore();
 
@@ -41,3 +41,5 @@ export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
     </div>
   );
 };
+
+export default Editor;
