ec61ced0646162bec0da3237d0c5904d69818c0d
Kyaw Thu <evanch98@gmail.com>
2023-10-31 13:20:07 +0800

    update the Cover component

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,5 +1,23 @@
 "use client";
 
-export const Cover = () => {
-  return <div>Cover</div>;
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+interface CoverProps {
+  url?: string;
+  preview?: boolean;
+}
+
+export const Cover = ({ url, preview }: CoverProps) => {
+  return (
+    <div
+      className={cn(
+        "relative w-full h-[35vh] group",
+        !url && "h-[12vh]",
+        url && "bg-muted"
+      )}
+    >
+      {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
+    </div>
+  );
 };
