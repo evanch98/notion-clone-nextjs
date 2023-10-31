025993722b27efe3c9579dcf2426bafb8bef76c7
Kyaw Thu <evanch98@gmail.com>
2023-10-31 13:27:56 +0800

    update the Cover component

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useCoverImage } from "@/hooks/use-cover-image";
 import { cn } from "@/lib/utils";
+import { ImageIcon, X } from "lucide-react";
 import Image from "next/image";
 
 interface CoverProps {
@@ -9,6 +12,8 @@ interface CoverProps {
 }
 
 export const Cover = ({ url, preview }: CoverProps) => {
+  const coverImage = useCoverImage();
+
   return (
     <div
       className={cn(
@@ -18,6 +23,28 @@ export const Cover = ({ url, preview }: CoverProps) => {
       )}
     >
       {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 2-4 mr-2" />
+            Change cover
+          </Button>
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <X className="h-4 2-4 mr-2" />
+            Remove
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
