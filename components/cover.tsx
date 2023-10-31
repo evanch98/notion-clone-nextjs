5e1709f44829e41db74986cd25714b2d5096db3d
Kyaw Thu <evanch98@gmail.com>
2023-10-31 22:09:21 +0800

    update the Cover component

--- components/cover.tsx
+++ components/cover.tsx
@@ -41,7 +41,7 @@ export const Cover = ({ url, preview }: CoverProps) => {
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
