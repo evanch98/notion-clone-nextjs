6e094b89f42c54ff7ff1b30e4744d942110000f5
Kyaw Thu <evanch98@gmail.com>
2023-10-31 11:48:44 +0800

    update the ModalProvider component

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CoverImageModal } from "@/components/modals/cover-image-modal";
 import { SettingsModal } from "@/components/modals/settings-modal";
 import { useEffect, useState } from "react";
 
@@ -17,6 +18,7 @@ export const ModalProvider = () => {
   return (
     <>
       <SettingsModal />
+      <CoverImageModal />
     </>
   );
 };
