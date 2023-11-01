bc1bd21977c42a509e35a9b3a17af4600e8d94e5
Kyaw Thu <evanch98@gmail.com>
2023-11-01 17:02:14 +0800

    update the Publish component

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -1,5 +1,11 @@
 "use client";
 
-export const Publish = () => {
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface PublishProps {
+  initialData: Doc<"documents">;
+}
+
+export const Publish = ({ initialData }: PublishProps) => {
   return <div>Publish</div>;
 };
