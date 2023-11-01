dcb378382197b6c63d2d314fd501236a9ff4984d
Kyaw Thu <evanch98@gmail.com>
2023-11-01 16:58:42 +0800

    update the Item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -121,7 +121,7 @@ export const Item = ({
       {documentIcon ? (
         <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+        <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
       )}
       <span className="truncate">{label}</span>
       {/* Render this part if and only if the "isSearched" prop is true */}
