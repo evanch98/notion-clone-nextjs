4b4a4d37c8039841a24863cb89556c70526b9ac7
Kyaw Thu <evanch98@gmail.com>
2023-11-01 17:35:35 +0800

    update the Publish component

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -1,11 +1,100 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
+import { useOrigin } from "@/hooks/use-origin";
+import { useMutation } from "convex/react";
+import { Globe } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 
 interface PublishProps {
   initialData: Doc<"documents">;
 }
 
 export const Publish = ({ initialData }: PublishProps) => {
-  return <div>Publish</div>;
+  const origin = useOrigin();
+  const update = useMutation(api.documents.update);
+
+  const [copied, setCopied] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const url = `${origin}/preview/${initialData._id}`;
+
+  const onPublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({ id: initialData._id, isPublished: true }).finally(
+      () => setIsSubmitting(false)
+    );
+
+    toast.promise(promise, {
+      loading: "Publishing...",
+      success: "Note published!",
+      error: "Failed to publish note.",
+    });
+  };
+
+  const onUnpublish = () => {
+    setIsSubmitting(true);
+
+    const promise = update({ id: initialData._id, isPublished: false }).finally(
+      () => setIsSubmitting(false)
+    );
+
+    toast.promise(promise, {
+      loading: "Unpublishing...",
+      success: "Note unpublished!",
+      error: "Failed to unpublish note.",
+    });
+  };
+
+  const onCopy = () => {
+    navigator.clipboard.writeText(url);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button size="sm" variant="ghost">
+          Publish
+          {initialData.isPublished && (
+            <Globe className="text-sky-500 w-4 h-4 ml-2" />
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
+        {initialData.isPublished ? (
+          <div>Published</div>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <Globe className="h-8 w-8 text-muted-foreground mb-2" />
+            <p className="text-sm font-medium mb-2">Publish this note</p>
+            <span className="text-xs text-muted-foreground mb-4">
+              Share your work with others.
+            </span>
+            <Button
+              disabled={isSubmitting}
+              onClick={onPublish}
+              className="w-full text-xs"
+              size="sm"
+            >
+              Publish
+            </Button>
+          </div>
+        )}
+      </PopoverContent>
+    </Popover>
+  );
 };
